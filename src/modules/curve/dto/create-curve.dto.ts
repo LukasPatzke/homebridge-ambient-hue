5cb08dd611e74fb81087968335cef1d94a4e3482
Lukas Patzke <lukas@patzke.me>
2023-01-06 11:17:33 +0000

    Add API Properties to DTOs

--- src/modules/curve/dto/create-curve.dto.ts
+++ src/modules/curve/dto/create-curve.dto.ts
@@ -1,7 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export type curveKind = 'ct' | 'bri';
 
 export class CreateCurveDto {
+  @ApiProperty()
   name: string;
+
+  @ApiProperty({enum: ['ct', 'bri']})
   kind: curveKind;
+
+  @ApiProperty({type: Number, required: false, default: 2})
   count = 2;
 }
--- src/modules/curve/dto/insert-point.dto.ts
+++ src/modules/curve/dto/insert-point.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class InsertPointDto {
+  @ApiProperty()
   index: number;
+
+  @ApiProperty({enum: ['before', 'after']})
   position: 'before' | 'after';
 }
