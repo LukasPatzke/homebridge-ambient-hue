eb51a3ea3cdac0e658b93794330b65192ed1d49f
Lukas Patzke <lukas@patzke.me>
2023-10-05 12:48:59 +0000

    Point DTO validation

--- src/modules/point/dto/create-point.dto.ts
+++ src/modules/point/dto/create-point.dto.ts
@@ -1,19 +1,30 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsOptional, Max, Min } from 'class-validator';
 import { BrightnessCurve } from '../../curve/entities/brightness.curve.entity';
 import { ColorTemperatureCurve } from '../../curve/entities/colorTemperature.curve.entity';
 
 export class CreatePointDto {
   @ApiProperty()
   @IsNumber()
+  @Min(0)
+  @Max(1440)
   x: number;
 
   @ApiProperty()
   @IsNumber()
+  @Min(0)
+  @Max(500)
   y: number;
 
+  @IsOptional()
   first?: boolean = false;
+
+  @IsOptional()
   last?: boolean = false;
+
+  @IsOptional()
   brightnessCurve?: BrightnessCurve;
+
+  @IsOptional()
   colorTemperatureCurve?: ColorTemperatureCurve;
 }
